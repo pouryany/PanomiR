8f6cf3f0714a880abb8aae3e45746bf5f5907981
Ilya Sytchev <isytchev@hsph.harvard.edu>
2021-11-19 11:18:22 -0500

    Update formatting for Bioc submission
    Former-commit-id: edcde039eacf4bf18dcde26bb7a254e7c59c224b [formerly 97da32611428fcfc823699bf8896216b41711641] [formerly f487a3cf2bcb90d385c3f2be71f2fdde817c427d [formerly 5fd801f0497cea05cdafa842e64b00f3032ed6cd]]
    Former-commit-id: 909859eb2c8910ac275210dad21513ff5cc71741
    Former-commit-id: 47104af8ae676175a344ff3eae7a82fda8b0de7e
    Former-commit-id: 0a43cf44fcd3ae6d88f8d74959822be6928252cc
    Former-commit-id: 21a18dc1257ee92dc9dcb7232932ddb35fe76b93
    Former-commit-id: d9936e5c5ac8fb43fac11f4a1b9ca86ff39bc63c [formerly bd6f772c6fe44f0764a6985f727fd7cba15746ab]
    Former-commit-id: 91db2a9d8d4638b446c528275702a80ab7066aab

--- R/mappingPathwaysClusters.R
+++ R/mappingPathwaysClusters.R
@@ -22,7 +22,6 @@
 #' @param weighted True if you wish to include correlation weights in clustering
 #' @return a table with each row containing a pathway and its respective cluster
 #' @export
-
 mappingPathwaysClusters <- function(pcxn,
                                     dePathways,
                                     clusteringFunction = NULL,
@@ -38,217 +37,223 @@ mappingPathwaysClusters <- function(pcxn,
                                     outDir = "",
                                     saveNameCSV = NULL,
                                     weighted = FALSE) {
-  if (substring(outDir, nchar(outDir)) != "/") {
-    outDir <- paste0(outDir, "/")
-  }
-  if (!dir.exists(outDir)) {
-    stop("Output directory does not exist.")
-  }
+    if (substring(outDir, nchar(outDir)) != "/") {
+        outDir <- paste0(outDir, "/")
+    }
+    if (!dir.exists(outDir)) {
+        stop("Output directory does not exist.")
+    }
+    
+    figDir <- paste0(outDir, "Figures/")
+    
+    if (!dir.exists(figDir)) {
+        dir.create(figDir, recursive = TRUE)
+    }
+    
+    figDir <- paste0(figDir, prefix)
+    
+    # filter pcxn edges with edge fdr threshold and correlation threshold
+    pcxn$p.Adjust <- stats::p.adjust(pcxn$p.value, method = "fdr")
+    res <- pcxn[pcxn$p.Adjust < edgeFDR, ]
+    res <- res[abs(res$PathCor) > correlationCutOff, ]
+    allPathways <- union(unique(res$Pathway.B), unique(res$Pathway.A))
+    net <- res[, c(1, 2, 4)]
+    if (weighted) {
+        net$weight <- abs(net$PathCor)
+    }
+    net <- as.matrix(net)
+    net <- igraph::graph_from_data_frame(net, directed = FALSE)
+    
+    igraph::set_vertex_attr(net, "col", value = "red")
+    
+    # filter de pathways with less than path fdr threshold
+    dePathways <- dePathways[dePathways$adj.P.Val < pathwayFDR, ]
+    if (nrow(dePathways) < 10) {
+      stop(
+        "Please use a more lenient threshold for pathway adjusted p-values."
+      )
+    }
 
-  figDir <- paste0(outDir, "Figures/")
-
-  if (!dir.exists(figDir)) {
-    dir.create(figDir, recursive = TRUE)
-  }
-
-  figDir <- paste0(figDir, prefix)
-
-  # filter pcxn edges with edge fdr threshold and correlation threshold
-  pcxn$p.Adjust <- stats::p.adjust(pcxn$p.value, method = "fdr")
-  res <- pcxn[pcxn$p.Adjust < edgeFDR, ]
-  res <- res[abs(res$PathCor) > correlationCutOff, ]
-  allPathways <- union(unique(res$Pathway.B), unique(res$Pathway.A))
-  net <- res[, c(1, 2, 4)]
-  if (weighted) {
-    net$weight <- abs(net$PathCor)
-  }
-  net <- as.matrix(net)
-  net <- igraph::graph_from_data_frame(net, directed = FALSE)
-
-  igraph::set_vertex_attr(net, "col", value = "red")
-
-  # filter de pathways with less than path fdr threshold
-  dePathways <- dePathways[dePathways$adj.P.Val < pathwayFDR, ]
-  if (nrow(dePathways) < 10) {
-    stop("Please use a more lenient threshold for pathway adjusted p-values.")
-  }
-
-  # choose top n pathways
-  if (!is.null(topPathways)) {
-    dePathways <- dePathways[seq_len(topPathways), ]
-  }
-  dePathways1 <- dePathways
-  dePathways  <- dePathways[which(rownames(dePathways) %in% allPathways), ]
-
-  igraph::V(net)$shape <- ifelse(igraph::V(net)$name %in% dePathways,
-                                 "square",
-                                 "circle")
-  subNet <- igraph::induced_subgraph(net, rownames(dePathways))
-
-  # choose clustering function for nodes
-  set.seed(seed)
-  if (is.null(clusteringFunction)) {
-    clusts <- igraph::cluster_edge_betweenness(subNet)
-  } else {
-    clusts <- clusteringFunction(subNet)
-  }
-
-  # setting up plot
-  zz <- as.factor(clusts$membership)
-  zz <- forcats::fct_infreq(zz, ordered = NA)
-  levels(zz) <- seq_along(unique(zz))
-  clusts$membership <- as.numeric(zz)
-
-  cols <- RColorBrewer::brewer.pal(8, "Set2")
-  
-  igraph::E(subNet)$color <-
-    ifelse(as.numeric(igraph::E(subNet)$PathCor) > 0, "#E41A1C", "#377EB8")
-  
-  shapeIndex <- rownames(dePathways1) %in% igraph::V(subNet)$name
-  shapeIndex <- dePathways1[shapeIndex, c(1, 2)]
-  shapeIndex <- shapeIndex[igraph::V(subNet)$name, ]
-
-  shapeDirs <- as.numeric(shapeIndex$logFC > 0)
-  names(shapeDirs) <- rownames(shapeIndex)
-  shapeIndex <- shapeIndex$logFC > 0
-
-  igraph::V(subNet)$shape <- ifelse(shapeIndex, "square", "circle")
-
-  if (plot == TRUE) {
-    legend_cats <- data.frame(
-      attr = c("Up-regulated", "Down-regulated"),
-      shape = unique(igraph::V(subNet)$shape)
-    )
-    nodeColors <- cols[clusts$membership]
-
-    small.clust <-
-      which(table(clusts$membership) <= table(clusts$membership)[5],
-            useNames = TRUE)
-
-    nodeColors[clusts$membership %in% small.clust] <- NA
-
-    grDevices::pdf(paste0(figDir, "PCxNCorGraph.pdf"), width = 18, height = 11)
+    # choose top n pathways
+    if (!is.null(topPathways)) {
+        dePathways <- dePathways[seq_len(topPathways), ]
+    }
+    dePathways1 <- dePathways
+    dePathways  <- dePathways[which(rownames(dePathways) %in% allPathways), ]
+    
+    igraph::V(net)$shape <- ifelse(igraph::V(net)$name %in% dePathways,
+                                   "square",
+                                   "circle")
+    subNet <- igraph::induced_subgraph(net, rownames(dePathways))
+    
+    # choose clustering function for nodes
     set.seed(seed)
-    plot(subNet,
-      vertex.size = 5, vertex.label = NA,
-      vertex.color = nodeColors
-    )
-    graphics::legend(
-      x = "bottomleft", # position, also takes x,y coordinates
-      legend = legend_cats$attr,
-      pch = c(0, 1),
-      bty = "n",
-      cex = 1.6
-    )
-    graphics::legend(
-      x = "topleft",
-      legend = c("Positive Cor", "Negative Cor"),
-      col = c("#E41A1C", "#377EB8"),
-      lty = 1,
-      lwd = 2,
-      cex = 1.6,
-      bty = "n"
-    )
-    grDevices::dev.off()
-  }
+    if (is.null(clusteringFunction)) {
+        clusts <- igraph::cluster_edge_betweenness(subNet)
+    } else {
+        clusts <- clusteringFunction(subNet)
+    }
+    
+    # setting up plot
+    zz <- as.factor(clusts$membership)
+    zz <- forcats::fct_infreq(zz, ordered = NA)
+    levels(zz) <- seq_along(unique(zz))
+    clusts$membership <- as.numeric(zz)
+    
+    cols <- RColorBrewer::brewer.pal(8, "Set2")
+    
+    igraph::E(subNet)$color <-
+        ifelse(as.numeric(igraph::E(subNet)$PathCor) > 0, "#E41A1C", "#377EB8")
+    
+    shapeIndex <- rownames(dePathways1) %in% igraph::V(subNet)$name
+    shapeIndex <- dePathways1[shapeIndex, c(1, 2)]
+    shapeIndex <- shapeIndex[igraph::V(subNet)$name, ]
+    
+    shapeDirs <- as.numeric(shapeIndex$logFC > 0)
+    names(shapeDirs) <- rownames(shapeIndex)
+    shapeIndex <- shapeIndex$logFC > 0
+    
+    igraph::V(subNet)$shape <- ifelse(shapeIndex, "square", "circle")
+    
+    if (plot == TRUE) {
+        legend_cats <- data.frame(
+            attr = c("Up-regulated", "Down-regulated"),
+            shape = unique(igraph::V(subNet)$shape)
+        )
+        nodeColors <- cols[clusts$membership]
+        
+        small.clust <-
+            which(table(clusts$membership) <= table(clusts$membership)[5],
+                  useNames = TRUE)
+        
+        nodeColors[clusts$membership %in% small.clust] <- NA
 
-  valTab <- as.data.frame(igraph::ends((subNet), igraph::E(subNet)))
-  valTab$cor <- ifelse(as.numeric(igraph::E(subNet)$PathCor) > 0, 1, 0)
-  valTab$V2 <- as.character(valTab$V2)
-  valTab$V1 <- as.character(valTab$V1)
+        grDevices::pdf(
+            paste0(figDir, "PCxNCorGraph.pdf"),
+            width = 18, height = 11
+        )
+        set.seed(seed)
+        plot(
+            subNet,
+            vertex.size = 5, vertex.label = NA,
+            vertex.color = nodeColors
+        )
+        graphics::legend(
+            x = "bottomleft", # position, also takes x,y coordinates
+            legend = legend_cats$attr,
+            pch = c(0, 1),
+            bty = "n",
+            cex = 1.6
+        )
+        graphics::legend(
+            x = "topleft",
+            legend = c("Positive Cor", "Negative Cor"),
+            col = c("#E41A1C", "#377EB8"),
+            lty = 1,
+            lwd = 2,
+            cex = 1.6,
+            bty = "n"
+        )
+        grDevices::dev.off()
+    }
+    
+    valTab <- as.data.frame(igraph::ends((subNet), igraph::E(subNet)))
+    valTab$cor <- ifelse(as.numeric(igraph::E(subNet)$PathCor) > 0, 1, 0)
+    valTab$V2 <- as.character(valTab$V2)
+    valTab$V1 <- as.character(valTab$V1)
+    
+    igraph::V(subNet)$shape <- ifelse(shapeIndex, "square", "circle")
 
-  igraph::V(subNet)$shape <- ifelse(shapeIndex, "square", "circle")
-
-
-  if (subplot == TRUE) {
-    for (k in seq_len(topClusters)) {
-      keep     <- which((clusts$membership) == k)
-      subNet2 <- igraph::induced_subgraph(subNet, keep)
-      
-      if (length(igraph::V(subNet2)) < 2) next
-
-      pathsOut <- igraph::V(subNet)$name
-
-      pathsOut <-
+    if (subplot == TRUE) {
+        for (k in seq_len(topClusters)) {
+            keep     <- which((clusts$membership) == k)
+            subNet2 <- igraph::induced_subgraph(subNet, keep)
+            
+            if (length(igraph::V(subNet2)) < 2) next
+            
+            pathsOut <- igraph::V(subNet)$name
+            
+            pathsOut <-
+                as.data.frame(cbind("Pathway" = pathsOut,
+                                    "cluster" = clusts$membership))
+            
+            grDevices::pdf(
+                paste0(figDir, "PCxNCorGraph_", "Cluster_", k, ".pdf")
+            )
+            
+            set.seed(seed + 1)
+            plot(subNet2,
+                 edge.width = 1.3, vertex.size = 5, vertex.label = NA,
+                 vertex.color = cols[clusts$membership[keep]],
+                 legend = TRUE,
+                 layout = igraph::layout.fruchterman.reingold
+            )
+            graphics::legend(
+                x = "bottomleft", # position, also takes x,y coordinates
+                legend = legend_cats$attr,
+                pch = c(0, 1),
+                bty = "n",
+                cex = 1.4
+            )
+            graphics::legend(
+                x = "topleft",
+                legend = c("Positive Cor", "Negative Cor"),
+                col = c("#E41A1C", "#377EB8"),
+                lty = 1,
+                lwd = 2,
+                cex = 1.4,
+                bty = "n"
+            )
+            grDevices::dev.off()
+        }
+    }
+    
+    "%notin%" <- Negate("%in%")
+    
+    remove <- which(table(clusts$membership) < 4)
+    remove <- which((clusts$membership %notin% remove))
+    
+    subNet2 <- igraph::induced_subgraph(subNet, remove)
+    
+    pathsOut <- igraph::V(subNet)$name
+    
+    pathsOut <-
         as.data.frame(cbind("Pathway" = pathsOut,
                             "cluster" = clusts$membership))
-
-      grDevices::pdf(paste0(figDir, "PCxNCorGraph_", "Cluster_", k, ".pdf"))
-
-      set.seed(seed + 1)
-      plot(subNet2,
-        edge.width = 1.3, vertex.size = 5, vertex.label = NA,
-        vertex.color = cols[clusts$membership[keep]],
-        legend = TRUE,
-        layout = igraph::layout.fruchterman.reingold
-      )
-      graphics::legend(
-        x = "bottomleft", # position, also takes x,y coordinates
-        legend = legend_cats$attr,
-        pch = c(0, 1),
-        bty = "n",
-        cex = 1.4
-      )
-      graphics::legend(
-        x = "topleft",
-        legend = c("Positive Cor", "Negative Cor"),
-        col = c("#E41A1C", "#377EB8"),
-        lty = 1,
-        lwd = 2,
-        cex = 1.4,
-        bty = "n"
-      )
-      grDevices::dev.off()
+    
+    if (!is.null(saveNameCSV)) {
+        utils::write.csv(pathsOut, paste0(outDir, saveNameCSV))
     }
-  }
-
-  "%notin%" <- Negate("%in%")
-
-  remove <- which(table(clusts$membership) < 4)
-  remove <- which((clusts$membership %notin% remove))
-
-  subNet2 <- igraph::induced_subgraph(subNet, remove)
-
-  pathsOut <- igraph::V(subNet)$name
-
-  pathsOut <-
-    as.data.frame(cbind("Pathway" = pathsOut,
-                        "cluster" = clusts$membership))
-
-  if (!is.null(saveNameCSV)) {
-    utils::write.csv(pathsOut, paste0(outDir, saveNameCSV))
-  }
-
-  if (subplot == TRUE) {
-    grDevices::pdf(paste0(figDir, "ConnectedPathways_PCxNCorGraph.pdf"),
-      width = 18, height = 11
-    )
-    set.seed(seed - 1)
-    plot(subNet2,
-      edge.width = 1.3, vertex.size = 5, vertex.label = NA,
-      vertex.color = cols[clusts$membership[remove]],
-      legend = TRUE,
-      layout = igraph::layout_components
-    )
-    graphics::legend(
-      x = "bottomright",
-      y = 200, ## position, also takes x,y coordinates
-      legend = legend_cats$attr,
-      pch = c(0, 1),
-      bty = "n",
-      cex = 1.4
-    )
-    graphics::legend(
-      x = "topleft",
-      legend = c("Positive Cor", "Negative Cor"),
-      col = c("#E41A1C", "#377EB8"),
-      lty = 1,
-      lwd = 2,
-      cex = 1.4,
-      bty = "n"
-    )
-    grDevices::dev.off()
-  }
-
-  return(pathsOut)
+    
+    if (subplot == TRUE) {
+        grDevices::pdf(paste0(figDir, "ConnectedPathways_PCxNCorGraph.pdf"),
+                       width = 18, height = 11
+        )
+        set.seed(seed - 1)
+        plot(subNet2,
+             edge.width = 1.3, vertex.size = 5, vertex.label = NA,
+             vertex.color = cols[clusts$membership[remove]],
+             legend = TRUE,
+             layout = igraph::layout_components
+        )
+        graphics::legend(
+            x = "bottomright",
+            y = 200, ## position, also takes x,y coordinates
+            legend = legend_cats$attr,
+            pch = c(0, 1),
+            bty = "n",
+            cex = 1.4
+        )
+        graphics::legend(
+            x = "topleft",
+            legend = c("Positive Cor", "Negative Cor"),
+            col = c("#E41A1C", "#377EB8"),
+            lty = 1,
+            lwd = 2,
+            cex = 1.4,
+            bty = "n"
+        )
+        grDevices::dev.off()
+    }
+    return(pathsOut)
 }
