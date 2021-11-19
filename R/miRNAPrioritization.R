fec996c88adc84578feefd16bb23901d8ce87347
Ilya Sytchev <isytchev@hsph.harvard.edu>
2021-11-19 11:26:57 -0500

    Update formatting for Bioc submission
    Former-commit-id: 3745545fdf3990a2cb8b95d84470b54bac7ad88d [formerly 4812e78fbad93aa8c6411d27978353272e0ea5f5] [formerly ec7a5e956f04d2725f711ac2e9dceefe0dc7ee8d [formerly 521e132d0f73a3831d05bd98c8feda747d1dddbb]]
    Former-commit-id: 2ec5681509ae2bd38002ad4b6e846d9e9a0cd62f
    Former-commit-id: 0991a90b620dbc29f0554c4ce65bdcb4c869e7e5
    Former-commit-id: 0e2c1f2cc964aebed48653b36cd6bf7417a39abb
    Former-commit-id: 562ea985fff68f461751b088502603d5be68ec6a
    Former-commit-id: 8da4dfff73b00c0037031fd0351eff1b6223e263 [formerly 0c28ca8aa2c5e5db9d37d5857ce94abb0b98a9f5]
    Former-commit-id: 456b9d797aa7a940e50d89c251d9a3c553792724

--- R/miRNAPrioritization.R
+++ R/miRNAPrioritization.R
@@ -43,189 +43,196 @@ prioritizeMicroRNA <- function(enriches0,
                                numCores = 1,
                                saveCSV = TRUE,
                                prefix = "") {
-  if (substring(outDir, nchar(outDir)) != "/") {
-    outDir <- paste0(outDir, "/")
-  }
-  if (!dir.exists(outDir)) {
-    warning("Output directory does not exist.")
-    dir.create(outDir, recursive = TRUE)
-  }
-
-  if (outDir == "/") {
-    outDir <- ""
-  }
-
-  if (substring(dataDir, nchar(dataDir)) != "/") {
-    dataDir <- paste0(dataDir, "/")
-  }
-  if (!dir.exists(dataDir)) {
-    stop("Data directory does not exist.")
-  }
-
-  if (dataDir == "/") {
-    dataDir <- ""
-  }
-
-  output <- list()
-
-  # count miRNA-pathway enrichment with p-value less than threshold
-  enriches <- enriches0 %>% dplyr::filter(., Intersect != 0)
-  enriches %<>% dplyr::group_by(., y) %>%
-    dplyr::mutate(., path_fdr = stats::p.adjust(pval,
-                                                method = "fdr"))
-  
-  enriches <- enriches %>%
-    dplyr::mutate(., hit = ifelse(path_fdr < enrichmentFDR, 1, 0))
-
-  # Need to fix this function to be able to work on specific clusters
-  # instead of all top clusters.
-  for (clustNo in seq_len(topClust)) {
-    clustName <- paste0("Cluster", clustNo)
-
-    print(paste0("Working on ", clustName, "."))
-
-    # select pathways in cluster
-    pathways <- 
-      as.character(pathwayClusters[pathwayClusters$cluster == clustNo,
-                                    ]$Pathway)
+    if (substring(outDir, nchar(outDir)) != "/") {
+        outDir <- paste0(outDir, "/")
+    }
+    if (!dir.exists(outDir)) {
+        warning("Output directory does not exist.")
+        dir.create(outDir, recursive = TRUE)
+    }
     
-    nPaths <- length(pathways)
-
-    # formulate number of miRNA-pathway enrichment with p-value less 
-    # than threshold for each miRNA
-    tempEnrich <- enriches[enriches$y %in% pathways, ]
-    selector <- tempEnrich %>%
-      dplyr::group_by(x) %>%
-      dplyr::summarise(., "cluster_hits" = sum(hit))
-
-    # perform p-value aggregation based on methodlogy provided
-    for (i in 1:length(method)) {
-      m <- method[i]
-
-      print(paste0("Performing ", m, " function."))
-      
-      fn      <- get(paste0(m, "Fn"))
-      coverFn <- get(paste0(m, "CoverFn"))
-
-      if (!is.null(methodThresh)) {
-        mThresh <- methodThresh[i]
-        temp <- fn(enriches = enriches0,
-                   pathways,
-                   isSelector = TRUE,
-                   thresh = mThresh)
-      } else {
-        temp <- fn(enriches = enriches0, pathways, isSelector = TRUE)
-      }
-
-      mSelector  <- temp$selector
-      mEnriches0 <- temp$enriches0
-
-      if (nrow(mSelector) < 3) {
-        print(paste0("Skipping ",
-                     m,
-                     " function due to low number of miRNA after filter"))
-        next
-      }
-
-      enrichNull <- mEnriches0 %>% dplyr::filter(., x %in% mSelector$x)
-
-      samplingDataDir <- paste0(dataDir, prefix, "Sampling_Data/")
-
-      if (saveSampling == TRUE) {
-        if (!dir.exists(samplingDataDir))
-          dir.create(samplingDataDir, recursive = TRUE)
-      }
-
-      samplingDataFilename <- paste0(prefix,
-                                       m,
-                                       "_",
-                                       sampRate,
-                                       "_samples.RDS")
-      
-      samplingDataFile <- paste0(samplingDataDir,
-                                   "/", 
-                                   samplingDataFilename)
-
-      if (m %in% c("aggInv", "aggLog")) {
-        # perform sampling
-        samplingData <- samplingDataBase(enrichNull,
-          mSelector,
-          sampRate,
-          fn,
-          nPaths,
-          samplingDataFile,
-          jackKnife = FALSE,
-          saveSampling = saveSampling,
-          numCores = 8
-        )
-
-        mSelector <- methodProbBase(
-          samplingData = samplingData[[paste0("SampSize_", 100)]],
-          selector = mSelector,
-          m = m,
-          nPaths = nPaths,
-          coverFn = coverFn
-        )
-      } else {
-        names(mSelector)[2:3] <- paste0(m, "_", names(mSelector)[2:3])
-      }
-
-
-      print(paste0(m, " Method Done"))
-
-      # perform jack-knife
-      if (runJackKnife == TRUE) {
-        samplingData <- samplingDataBase(enrichNull,
-          mSelector,
-          sampRate,
-          fn,
-          nPaths,
-          samplingDataFile,
-          jackKnife = FALSE,
-          saveSampling = saveSampling,
-          numCores = 8
-        )
-        mSelector <- jackKnifeBase(
-          selector = mSelector,
-          pathways = pathways,
-          enrichNull = enrichNull,
-          fn = fn,
-          jackKnifeData = samplingData[[paste0("SampSize_", 100)]],
-          m = m,
-          numCores = 8
-        )
-
-        print(paste0(m, " JackKnifing Method Done!"))
-
-        mSelector <- mSelector[, c(1, 3, 2, 4)]
-      } else {
-        mSelector <- mSelector[, c(1, 3, 2)]
-      }
-
-      selector <- merge(selector, mSelector, all = TRUE)
-
-      met <- paste0(m, "_pval")
-      selector <- selector %>% dplyr::arrange(., !!rlang::sym(met))
-      met2 <- paste0(m, "_fdr")
-      selector <- selector %>% dplyr::mutate(
-        .,
-        !!met2 := stats::p.adjust(p = !!rlang::sym(met),method = "fdr")
-      )
+    if (outDir == "/") {
+        outDir <- ""
     }
-
-    if (saveCSV == TRUE) {
-      saveName <- paste0(prefix,
-                          sampRate,
-                          "_samples_clustNo_",
-                          clustNo, ".csv")
-      
-      print(paste0(saveName, " saved!"))
-      
-      utils::write.csv(selector, paste0(outDir, saveName))
+    
+    if (substring(dataDir, nchar(dataDir)) != "/") {
+        dataDir <- paste0(dataDir, "/")
     }
+    if (!dir.exists(dataDir)) {
+        stop("Data directory does not exist.")
+    }
+    
+    if (dataDir == "/") {
+        dataDir <- ""
+    }
+    
+    output <- list()
+    
+    # count miRNA-pathway enrichment with p-value less than threshold
+    enriches <- enriches0 %>% dplyr::filter(., Intersect != 0)
+    enriches %<>% dplyr::group_by(., y) %>%
+        dplyr::mutate(., path_fdr = stats::p.adjust(pval, method = "fdr"))
 
-    output[[clustName]] <- selector
-  }
+    enriches <- enriches %>%
+        dplyr::mutate(., hit = ifelse(path_fdr < enrichmentFDR, 1, 0))
+    
+    # Need to fix this function to be able to work on specific clusters
+    # instead of all top clusters.
+    for (clustNo in seq_len(topClust)) {
+        clustName <- paste0("Cluster", clustNo)
+        
+        print(paste0("Working on ", clustName, "."))
+        
+        # select pathways in cluster
+        pathways <- as.character(
+            pathwayClusters[pathwayClusters$cluster == clustNo, ]$Pathway
+        )
+        nPaths <- length(pathways)
+        
+        # formulate number of miRNA-pathway enrichment with p-value less
+        # than threshold for each miRNA
+        tempEnrich <- enriches[enriches$y %in% pathways, ]
+        selector <- tempEnrich %>%
+            dplyr::group_by(x) %>%
+            dplyr::summarise(., "cluster_hits" = sum(hit))
+        
+        # perform p-value aggregation based on methodlogy provided
+        for (i in 1:length(method)) {
+            m <- method[i]
+            
+            print(paste0("Performing ", m, " function."))
+            
+            fn <- get(paste0(m, "Fn"))
+            coverFn <- get(paste0(m, "CoverFn"))
+            
+            if (!is.null(methodThresh)) {
+                mThresh <- methodThresh[i]
+                temp <- fn(
+                    enriches = enriches0,
+                    pathways,
+                    isSelector = TRUE,
+                    thresh = mThresh
+                )
+            } else {
+                temp <- fn(enriches = enriches0, pathways, isSelector = TRUE)
+            }
+            
+            mSelector <- temp$selector
+            mEnriches0 <- temp$enriches0
+            
+            if (nrow(mSelector) < 3) {
+                print(paste0(
+                    "Skipping ",
+                    m,
+                    " function due to low number of miRNA after filter"
+                ))
+                next
+            }
+            
+            enrichNull <- mEnriches0 %>% dplyr::filter(., x %in% mSelector$x)
+            
+            samplingDataDir <- paste0(dataDir, prefix, "Sampling_Data/")
+            
+            if (saveSampling == TRUE) {
+                if (!dir.exists(samplingDataDir)) {
+                    dir.create(samplingDataDir, recursive = TRUE)
+                }
+            }
+            
+            samplingDataFilename <- paste0(
+                prefix,
+                m,
+                "_",
+                sampRate,
+                "_samples.RDS"
+            )
 
-  return(output)
+            samplingDataFile <- paste0(
+                samplingDataDir,
+                "/",
+                samplingDataFilename
+            )
+            
+            if (m %in% c("aggInv", "aggLog")) {
+                # perform sampling
+                samplingData <- samplingDataBase(enrichNull,
+                                                 mSelector,
+                                                 sampRate,
+                                                 fn,
+                                                 nPaths,
+                                                 samplingDataFile,
+                                                 jackKnife = FALSE,
+                                                 saveSampling = saveSampling,
+                                                 numCores = 8
+                )
+                
+                mSelector <- methodProbBase(
+                    samplingData = samplingData[[paste0("SampSize_", 100)]],
+                    selector = mSelector,
+                    m = m,
+                    nPaths = nPaths,
+                    coverFn = coverFn
+                )
+            } else {
+                names(mSelector)[2:3] <- paste0(m, "_", names(mSelector)[2:3])
+            }
+            
+            print(paste0(m, " Method Done"))
+            
+            # perform jack-knife
+            if (runJackKnife == TRUE) {
+                samplingData <- samplingDataBase(enrichNull,
+                                                 mSelector,
+                                                 sampRate,
+                                                 fn,
+                                                 nPaths,
+                                                 samplingDataFile,
+                                                 jackKnife = FALSE,
+                                                 saveSampling = saveSampling,
+                                                 numCores = 8
+                )
+                mSelector <- jackKnifeBase(
+                    selector = mSelector,
+                    pathways = pathways,
+                    enrichNull = enrichNull,
+                    fn = fn,
+                    jackKnifeData = samplingData[[paste0("SampSize_", 100)]],
+                    m = m,
+                    numCores = 8
+                )
+                
+                print(paste0(m, " JackKnifing Method Done!"))
+                
+                mSelector <- mSelector[, c(1, 3, 2, 4)]
+            } else {
+                mSelector <- mSelector[, c(1, 3, 2)]
+            }
+            
+            selector <- merge(selector, mSelector, all = TRUE)
+            
+            met <- paste0(m, "_pval")
+            selector <- selector %>% dplyr::arrange(., !!rlang::sym(met))
+            met2 <- paste0(m, "_fdr")
+            selector <- selector %>% dplyr::mutate(
+                .,
+                !!met2 := stats::p.adjust(p = !!rlang::sym(met), method = "fdr")
+            )
+        }
+        
+        if (saveCSV == TRUE) {
+            saveName <- paste0(
+                prefix,
+                sampRate,
+                "_samples_clustNo_",
+                clustNo, ".csv"
+            )
+            
+            print(paste0(saveName, " saved!"))
+            
+            utils::write.csv(selector, paste0(outDir, saveName))
+        }
+        
+        output[[clustName]] <- selector
+    }
+    return(output)
 }
