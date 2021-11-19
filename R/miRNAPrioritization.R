e218f9eee85338cfb145636589947a27c632437f
Pourya Naderi <pourya@ALD4E4AA.its.caregroup.org>
2021-11-19 12:12:26 -0500

    fixing sumz sumlog error
    Former-commit-id: df1168699d4de73d08b205c89b70009e4ed86f6d [formerly ad646fdacdfeb0c03df58cc5d3d2c27777aa6b48] [formerly 312089272ae4f94bb8b2d360447542a93f2810fe [formerly 85a083a7ceba33abde7a99cb14e0f37d615b6fd0]]
    Former-commit-id: 8516c56945bda297a261ee806f99a511488b26a8
    Former-commit-id: 8f278848d9bedf7523a66fc4a202508f6aad2db3
    Former-commit-id: 3be539853ced139ae847e4f5e3d5b79180cae330
    Former-commit-id: e7d34f25a74e4e00c7a7dd2f882db460fb6b774e
    Former-commit-id: 9011e2dc0086ee6a86e2e56bd5bc498e4a68c984 [formerly ae130dc01731979c22571d2e3e779b23b7139bb0]
    Former-commit-id: afcedae0ff7d900a4058e232707506d69d8b4164

--- R/miRNAPrioritization.R
+++ R/miRNAPrioritization.R
@@ -180,7 +180,7 @@ prioritizeMicroRNA <- function(enriches0,
             print(paste0(m, " Method Done"))
             
             # perform jack-knife
-            if ((runJackKnife == TRUE) && (me != "sumz") && (me != "sumlog")) {
+            if ((runJackKnife == TRUE) && (m != "sumz") && (me != "sumlog")) {
                 samplingData <- samplingDataBase(enrichNull,
                                                  mSelector,
                                                  sampRate,
