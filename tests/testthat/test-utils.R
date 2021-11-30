f66a165b1f51150232ef811a72b2184fc3c8a801
Pourya Naderi <pourya@ALD4E4AA.its.caregroup.org>
2021-11-29 23:21:44 -0500

    addressing lint errors

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -3,19 +3,19 @@ test_that("multiplication works", {
 })
 
 test_that("Pathway-gene table works", {
-    Pathway1 <- c("125", "3099", "126")
-    Pathway2 <- c("5232", "5230", "5162")
-    path.list <- list("Path1" = Pathway1, "Path2" = Pathway2)
+    pathway1 <- c("125", "3099", "126")
+    pathway2 <- c("5232", "5230", "5162")
+    pathList <- list("Path1" = pathway1, "Path2" = pathway2)
 
     # org.Hs.eg.db is required
-    res <- pathwayGeneTab(pathwayList = path.list)
+    res <- pathwayGeneTab(pathwayList = pathList)
     expect_equal(nrow(res), 6)
 })
 
 
 test_that("Pathway summary generates a correct number of pathways", {
 
-    path_tab <- tibble::tribble(
+    pathTab <- tibble::tribble(
         ~Pathway, ~ENTREZID,  ~ENSEMBL,
         "Path1",   "125",      "ENSG00000196616",
         "Path1",   "3099",     "ENSG00000159399",
@@ -24,10 +24,10 @@ test_that("Pathway summary generates a correct number of pathways", {
     )
 
     exprsMat <- matrix(2 * (seq_len(12)), 4, 3)
-    rownames(exprsMat) <- path_tab$ENSEMBL
+    rownames(exprsMat) <- pathTab$ENSEMBL
     colnames(exprsMat) <- LETTERS[seq_len(3)]
 
-    res <- pathwaySummary(exprsMat, path_tab, method = "x2")
+    res <- pathwaySummary(exprsMat, pathTab, method = "x2")
 
     expect_equal(nrow(res), 2)
 })
@@ -35,7 +35,7 @@ test_that("Pathway summary generates a correct number of pathways", {
 
 test_that("Pathway summary generates a correct number of samples", {
 
-    path_tab <- tibble::tribble(
+    pathTab <- tibble::tribble(
         ~Pathway, ~ENTREZID,  ~ENSEMBL,
         "Path1",   "125",      "ENSG00000196616",
         "Path1",   "3099",     "ENSG00000159399",
@@ -44,10 +44,10 @@ test_that("Pathway summary generates a correct number of samples", {
     )
 
     exprsMat <- matrix(2 * seq_len(1:12), 4, 3)
-    rownames(exprsMat) <- path_tab$ENSEMBL
+    rownames(exprsMat) <- pathTab$ENSEMBL
     colnames(exprsMat) <- LETTERS[seq_len(3)]
 
-    res <- pathwaySummary(exprsMat, path_tab, method = "x2")
+    res <- pathwaySummary(exprsMat, pathTab, method = "x2")
 
     expect_equal(ncol(res), 3)
 })
@@ -55,7 +55,7 @@ test_that("Pathway summary generates a correct number of samples", {
 
 test_that("Pathway summary generates correct activity scores", {
 
-    path_tab <- tibble::tribble(
+    pathTab <- tibble::tribble(
         ~Pathway, ~ENTREZID,  ~ENSEMBL,
         "Path1",   "125",      "ENSG00000196616",
         "Path1",   "3099",     "ENSG00000159399",
@@ -64,10 +64,10 @@ test_that("Pathway summary generates correct activity scores", {
     )
 
     exprsMat <- matrix(2 * (1:12), 4, 3)
-    rownames(exprsMat) <- path_tab$ENSEMBL
+    rownames(exprsMat) <- pathTab$ENSEMBL
     colnames(exprsMat) <- LETTERS[1:3]
 
-    res <- pathwaySummary(exprsMat, path_tab, method = "x2")
+    res <- pathwaySummary(exprsMat, pathTab, method = "x2")
 
     expect_equal(res[1, 1], 2.5)
     expect_equal(res[2, 1], 12.5)
